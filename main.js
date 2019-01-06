7b49be518fd61dc6f70da04baf4563772316c7b7
rika0384 <sora6kuro0f9@yahoo.co.jp>
2019-01-06 17:13:40 +0900

    add yukicoder ac

--- main.js
+++ main.js
@@ -299,12 +299,13 @@ function getCodeForces(handle){
             }
 
         });
-    
+
 }
 function getYukicoder(handle){
     var solved = 0;
     var new_ac = 0;
-    var yukicoder_ac = {"1511779777":1,"1546276642":1,"1546414885":1,"1546511280":1,"1546537883":1,"1546626859":1};
+    var yukicoder_ac = {"1511779777":1,"1546276642":1,"1546414885":1,"1546511280":1,"1546537883":1,"1546626859":1,
+                        "1546762161":1};
     for(var key in yukicoder_ac){
         all_ac[key] = 1;
          if(Number(key) >= new_time)new_ac++;
